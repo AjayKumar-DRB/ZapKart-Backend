e1799bf082265a068d857896fd1c3d482c49b5af
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2024-09-25 18:20:22 +0530

    Update db connection

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const databaseURL = process.env.DATABASE_URL
+const databaseURL = 'mongodb+srv://ajaykumardrb:BXFljrgrnMqGDVRj@goldratecalculator.3twdz5b.mongodb.net/'
 
 app.use(
   cors({
