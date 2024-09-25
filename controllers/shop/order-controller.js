923d7f9c073483c6be85a521030fec5f270b6cd8
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2024-09-26 00:16:20 +0530

    edit payment baseURL

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ export const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: "https://zapcart.netlify.app/shop/paypal-return",
+        cancel_url: "https://zapcart.netlify.app/shop/paypal-cancel",
       },
       transactions: [
         {
